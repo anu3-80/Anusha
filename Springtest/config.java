0c1ba68fa09eed7ad1f0d36364dc13e8aaa91ec5
anu3-80 <113579546+anu3-80@users.noreply.github.com>
2024-12-04 17:07:38 +0530

    Create config.java

---
+++ Springtest/config.java
@@ -0,0 +1,26 @@
+package Spring_test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config 
+{
+	@Bean
+	public Student stu()
+	{
+		Student s=new Student();
+		s.setName("Anusha");
+		s.setSectioon("A");
+		s.setMarks(45);
+		s.setClg("Sviet");
+		return s;
+	}
+	@Bean
+	public Employee emp()
+	{
+		Employee e=new Employee(1,"Kishore","Technical Dept",25000);
+		return e;
+	}
+	
+}
